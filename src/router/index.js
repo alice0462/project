76294c20680463dbd9d73a1115c9de47306a7dca
Alice Gelotte <alice.gelotte.0462@student.uu.se>
2024-12-05 14:07:09 +0100

    alice uppdatering

--- src/router/index.js
+++ src/router/index.js
@@ -32,4 +32,6 @@ const router = createRouter({
   ]
 })
 
+
+
 export default router
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -39,16 +39,30 @@
     </button>
   </router-link>
   
-  <a href="">
-    <button class="mainButtons" id="aboutClass">
+  
+    <button class="mainButtons" id="aboutClass" @click.prevent="showRulesText = true">
       {{ uiLabels.about }}  
     </button>
-  </a>
-  
+
+
+<div v-if="showRulesText" class="showRulesTextClass">
+  <div class="showRulesTextClass-content">
+    <span class="close" @click="closeRulesText">&times;</span>
+    <h3>Spelregler</h3>
+    <p>Vi besöker en okänd destination, en stad i världen, och ditt jobb är att lista ut vart vi är påväg!</p>
+    <p>Du får höra ett antal ledtrådar ledtrådar värda 10p, 8p, 6p, 4p och 2p. Uppe i högra hörnet ser du en timer och du måste svara inom den givna tiden. När du tror du vet destinationen, drar du i nödbromsen och skriver in ditt svar!</p>
+    <p>När destinationen är officiell, ska du svara på två frågor kopplade till destinationen. Rätt svar på 1 fråga ger 1 poäng, rätt svar på 2 frågor ger 3 poäng. Den med flest poäng när spelet avslutas vinner!</p>
+  </div>
+</div>
+
+
 </body>
 </template>
 
 <script>
+
+
+
 import ResponsiveNav from '@/components/ResponsiveNav.vue';
 import io from 'socket.io-client';
 const socket = io("localhost:3000");
@@ -62,8 +76,10 @@ export default {
     return {
       uiLabels: {},
       newPollId: "",
+      somePollId: "",
       lang: localStorage.getItem( "lang") || "en",
-      hideNav: true
+      hideNav: true,
+      showRulesText: false
     }
   },
   created: function () {
@@ -83,6 +99,9 @@ export default {
     },
     toggleNav: function () {
       this.hideNav = ! this.hideNav;
+    },
+    closeRulesText () {
+      this.showRulesText = false;
     }
   }
 }
@@ -154,12 +173,12 @@ export default {
   width: 300px;
   border-radius: 20px;
   margin: 70px 20px;
-  
+  cursor: pointer;
 }
+
 #createPollClass {
   background: linear-gradient(5deg, #ffa82f, #eaceaf);
 }
-
 #participatePollClass {
   background: linear-gradient(5deg, #47a657, #bbf8bb);
 }
@@ -176,11 +195,48 @@ export default {
   width:150px;
   justify-self: end;
   margin: 10px;
+  cursor: pointer;
 }
-
 .imageOfWorld {
 height: 27%;
 width: 27%;
 }
 
+
+
+
+.showRulesTextClass {
+  position: fixed;
+  z-index: 1; 
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.showRulesTextClass-content {
+  background-color: #95b3bf; 
+  padding: 20px;
+  border-radius: 15px; 
+  width: 60%; 
+  max-width: 700px; 
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); /* Skugga */
+  color: black;
+  font-size: 1.1rem;
+  position: relative;
+}
+
+.close {
+  color: #333;
+  font-size: 24px;
+  font-weight: bold;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
 </style>
