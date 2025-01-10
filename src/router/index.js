ce1ec70ceb8d8c8e60a06d42aeb8d7ba276af1cb
smarrolina <carolina.cuevas.6874@student.uu.se>
2025-01-10 15:20:26 +0100

    Carro

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import StartView from '../views/StartView.vue'
 import RandomDestination from "../components/RandomDestination.vue";
 import ChooseDestination from "../components/ChooseDestination.vue";
 import goToCode from '../components/goToCode.vue';
-import GameMasterCode from '../components/GameMasterCode.vue';
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -3,10 +3,10 @@
     <div class="lobby-info">
       <h1> {{ gameCode }}: <span class="pollId"> {{ this.pollId }} </span></h1>
       <h2> {{ participantsLabel }}:</h2>
-      <!--{{ this.participants }} TROR INTE DENNA BEHÖVS FÖR VI HAR JU DEN NUMRERAD PÅ RAD 9-->
-      <!-- HÄR SKA VI KOPPLA IHOP DE NAMN SOM SKRIVS IN I PLAYERS!!!!-->
-      <p v-for="(participant, index) in participants" :key="index">
-            {{ participant.name }}</p>
+      <div class="participant-list">
+        <p v-for="(participant, index) in participants" :key="index">
+          {{ participant.name }}</p>
+      </div>
     </div>
     <div class="game-visual">
       <img src="https://www.wallquotes.com/sites/default/files/kids0017-18.png"
@@ -15,66 +15,69 @@
     </div>
   </div>
 </template>
+
+
  <script>
-import { useRoute } from 'vue-router';
-import io from 'socket.io-client';
-const socket = io("localhost:3000");
-import playersSV from "/src/assets/players-sv.json";
-import playersEN from "/src/assets/players-en.json";
 
-
-
-
-export default {
+ import io from 'socket.io-client';
+ const socket = io("localhost:3000");
+ import playersSV from "/src/assets/players-sv.json";
+ import playersEN from "/src/assets/players-en.json";
+ 
+ export default {
   name: "Lobby",
   data: function () {
-      return {
-          pollId: "",
-          //uiLabels: {},
-          lang: localStorage.getItem("lang") || "en",
-          participants: [],
-          selectToGamecode: "",
-          selectedCities: [],
-          role: localStorage.getItem("role"), //Hämtar den tilldelade rollen som bestäms startView
-      }
+    return {
+      pollId: "",
+      lang: localStorage.getItem("lang") || "en",
+      participants: [],
+      selectToGamecode: "",
+      selectedCities: [],
+      role: localStorage.getItem("role"), //Hämtar den tilldelade rollen som bestäms startView
+    }
   },
+
   created: function () {
-      this.pollId = this.$route.params.id;
-      socket.on("gameStarted", () => {
-        console.log("Din roll är:", this.role);
-        if (this.role === "admin") {
-          this.$router.push("/answers/" + this.pollId);
-        } else if (this.role === "player") {
-          this.$router.push("/participant-answer/" + this.pollId);
-        } else if (this.role === "screen"){
-          this.$router.push("/game/" + this.pollId);
-        }
-      });
-      socket.on('participantsUpdate', (p) => {
-          console.log("mottagna deltagare:", p);
-          this.participants = p; });
-      socket.on("selectedToGameCode", (data) => {
+    this.pollId = this.$route.params.id;
+    socket.on("gameStarted", () => {
+      console.log("Din roll är:", this.role);
+      if (this.role === "admin") {
+        this.$router.push("/answers/" + this.pollId);
+      } else if (this.role === "player") {
+        this.$router.push("/participant-answer/" + this.pollId);
+      } else if (this.role === "screen"){
+        this.$router.push("/game/" + this.pollId);
+      }
+    });
+    
+    socket.on('participantsUpdate', (p) => {
+      console.log("mottagna deltagare:", p);
+        this.participants = p; 
+    });
+
+    socket.on("selectedToGameCode", (data) => {
       console.log("Mottagen nivå:", data);
-        // Spara städerna i data
+      // Spara städerna i data
       this.selectedToGameCode = data.level;
       this.selectedCities = data.cities;
-      });
-      socket.emit("joinPoll", this.pollId);
-      socket.emit("getParticipants", this.pollId);
-    },
+    });
+
+    socket.emit("joinPoll", this.pollId);
+    socket.emit("getParticipants", this.pollId);
+  },
 
 
-    computed: {
-      gameCode() {
-        return this.lang === "sv" ? playersSV.gameCode : playersEN.gameCode;
-      },
-      participantsLabel() {
-        return this.lang === "sv" ? playersSV.participantsLabel : playersEN.participantsLabel
-      },
-      letsGo(){
-        return this.lang === "sv" ? playersSV.letsGo : playersEN.letsGo
-      }
+  computed: {
+    gameCode() {
+      return this.lang === "sv" ? playersSV.gameCode : playersEN.gameCode;
     },
+    participantsLabel() {
+      return this.lang === "sv" ? playersSV.participantsLabel : playersEN.participantsLabel
+    },
+    letsGo(){
+      return this.lang === "sv" ? playersSV.letsGo : playersEN.letsGo
+    }
+  },
    
   methods: {
     startGame() {
@@ -82,17 +85,11 @@ export default {
     }
   },
 }
-
-
-
-
-
-
-
-
 </script>
+
+
  <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+ @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
 
 .lobby-container {
   display: flex;
@@ -111,13 +108,24 @@ export default {
   justify-content: center;
   align-items: center;
   width: 100%;
-  text-align: center; /* Centrerar texten i mitten */
+  text-align: center; 
 }
  h1 {
   font-size: clamp(1.5rem, 3vw, 2.5rem);
   color: #333;
   position: absolute;
   top: 10%;
+}
+
+.participant-list {
+  text-align: center; 
+  margin-top: -300px;
+}
+
+.participant-list p {
+  margin: 5px 0; 
+  font-size: 1rem; 
+  color: #333;
 }
  .pollId {
   font-weight: bold;
@@ -130,24 +138,20 @@ export default {
   position: absolute;
   top: 20%;
 }
- ol {
-  font-size: 1.2em;
-  line-height: 1.8;
-}
  .game-visual {
-  flex-direction: column; /* Placera tåget ovanför knappen */
-  justify-content: center; /* Centrerar innehåll vertikalt */
-  align-items: center; /* Centrerar innehåll horisontellt */
-  gap: 20px; /* Lägger mellanrum mellan tåget och knappen */
-  width: 100%; /* Låt sektionen ta upp hela bredden */
+  flex-direction: column; 
+  justify-content: center; 
+  align-items: center; 
+  gap: 20px; 
+  width: 100%; 
 
   display: flex;
 }
  .train-image {
-  width: clamp(40%, 50vw, 70%); /* Dynamisk storlek för tåget */
+  width: clamp(100%, 50vw, 50%);
   max-height: clamp(20vh, 50vh, 60vh);
-  object-fit: contain; /* Behåll proportionerna */
-  display: block; /* För säkerhets skull, se till att bilden är blocknivå */
+  object-fit: contain; 
+  display: block; 
   justify-content: center;
   position: absolute;
   top: 45%;
@@ -156,7 +160,7 @@ export default {
  .start-button {
   background: linear-gradient(5deg, #63b65f, #bcedbe);
   color: #00350f;
-  font-size: clamp(1.5rem, 1.5vw, 1.5rem); /* Dynamisk storlek för text */
+  font-size: clamp(1.5rem, 1.5vw, 1.5rem); 
   font-family: 'Futura', sans-serif;
   padding: clamp(20px, 3vw, 40px) clamp(40px, 6vw, 100px); 
   border: 2px solid #00720f;
