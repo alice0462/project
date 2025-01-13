ed406609b2029e93f98509631c239cc156e8c5d5
Alice Gelotte <alice.gelotte.0462@student.uu.se>
2025-01-13 16:54:13 +0100

    städ

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -45,7 +45,7 @@ import io from 'socket.io-client';
 sessionStorage.setItem("currentNetwork","localhost:3000") //detta var den ursprungliga servern
 //sessionStorage.setItem("currentNetwork", "130.243.218.223:3000") //Eduroam
 //sessionStorage.setItem("currentNetwork","130.243.148.112:3000") //UUGuest 
-sessionStorage.setItem("currentNetwork", "172.20.10.2:3000") //edvin internetdelnig
+//sessionStorage.setItem("currentNetwork", "172.20.10.2:3000") //edvin internetdelnig
 //sessionStorage.setItem("currentNetwork", "172.20.10.3:3000") //sara internetdelnig
 //sessionStorage.setItem("currentNetwork", "172.20.10.3:3000") //Carros internetdelning
 //sessionStorage.setItem("currentNetwork", "172.20.10.8:3000") //Alice internetdelning
@@ -131,20 +131,13 @@ export default {
 
 <style scoped>
   html, body {
-    margin: 0;
-    padding: 0;
     width: 100%;
-    height: 100%; /* Täck hela höjden */
-    background: linear-gradient(5deg, rgb(123, 168, 205), #d3d3f5); /* Gradientbakgrund */
-    background-size: cover; /* Se till att täcka hela ytan */
+    height: 100%; 
+    background: linear-gradient(5deg, rgb(123, 168, 205), #d3d3f5); 
+    background-size: cover; 
     background-repeat: no-repeat;
     overflow: hidden;
-  }
-  body{
-    background: linear-gradient(5deg, rgb(123, 168, 205), #d3d3f5);
-    height: 100%; 
     min-height: 100vh;
-    width: 100%;  
   }
   header {
     background: linear-gradient(5deg, #d3d3f5), rgb(123, 168, 205);
@@ -287,102 +280,95 @@ export default {
     background: linear-gradient(5deg, #fae0fad8, #fdc8ecb1);
     justify-self: end;
   }
-.mainButtons {
-  color: rgb(79, 80, 80);
-  font-family: "futura", sans-serif;
-  font-size: 30px;
-  height: 120px;
-  width: 300px;
-  border-radius: 20px;
-  margin: 0 20px 20px;
-  cursor: pointer;
-}
+  .mainButtons {
+    color: rgb(79, 80, 80);
+    font-family: "futura", sans-serif;
+    font-size: 30px;
+    height: 120px;
+    width: 300px;
+    border-radius: 20px;
+    margin: 0 20px 20px;
+    cursor: pointer;
+  }
+  #createPollClass {
+    background: linear-gradient(5deg, #ffa82f, #eaceaf);
+  }
+  #participatePollClass {
+    background: linear-gradient(5deg, #47a657, #bbf8bb);
+  }
+  #rulesClass {
+    background: linear-gradient(5deg, #ad5c99, #fdc8ec);
+  }
+  #screenViewerClass {
+    background: linear-gradient(5deg, #4a90e2, #a4d3f8); 
+  }
 
-#createPollClass {
-  background: linear-gradient(5deg, #ffa82f, #eaceaf);
-}
-#participatePollClass {
-  background: linear-gradient(5deg, #47a657, #bbf8bb);
-}
-#rulesClass {
-  background: linear-gradient(5deg, #ad5c99, #fdc8ec);
-}
-#screenViewerClass {
-  background: linear-gradient(5deg, #4a90e2, #a4d3f8); 
-}
-
-.languageButtonClass {
-  padding: 10px;
-  margin: 0;
-  overflow: hidden; 
-  border: none; 
-  width: 100px; 
-  height: 60px; 
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  background: none; 
-}
-
-.imageOfWorld {
-height: 27%;
-width: 27%;
-margin-bottom: 60px;
-}
-
-.showRulesTextClass {
-  position: fixed;
-  z-index: 1; 
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: "futura";
-}
-
-.showRulesTextClass-content {
-  background-color: #d2e0e6; 
-  padding: 20px;
-  border-radius: 15px; 
-  width: 60%; 
-  max-width: 700px; 
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); /* Skugga */
-  color: black;
-  font-size: 1.1rem;
-  position: relative;
-}
-
-.close {
-  color: #333;
-  font-size: 24px;
-  font-weight: bold;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  cursor: pointer;
-}
-.languageButtonClass img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.ruleButton {
-  color: rgb(79, 80, 80);
-  font-family: "futura";
-  font-size: 14px;
-  height: 50px;
-  width: 120px;
-  border-radius: 5px;
-  margin: 15px;
-  cursor: pointer;
-  background: linear-gradient(5deg, #fae0fad8, #fdc8ecb1);
-  justify-self: end;
-}
-
+  .languageButtonClass {
+    padding: 10px;
+    margin: 0;
+    overflow: hidden; 
+    border: none; 
+    width: 100px; 
+    height: 60px; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    background: none; 
+  }
+  .imageOfWorld {
+    height: 27%;
+    width: 27%;
+    margin-bottom: 60px;
+  }
+  .showRulesTextClass {
+    position: fixed;
+    z-index: 1; 
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "futura";
+  }
+  .showRulesTextClass-content {
+    background-color: #d2e0e6; 
+    padding: 20px;
+    border-radius: 15px; 
+    width: 60%; 
+    max-width: 700px; 
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); /* Skugga */
+    color: black;
+    font-size: 1.1rem;
+    position: relative;
+  }
+  .close {
+    color: #333;
+    font-size: 24px;
+    font-weight: bold;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+  }
+  .languageButtonClass img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .ruleButton {
+    color: rgb(79, 80, 80);
+    font-family: "futura";
+    font-size: 14px;
+    height: 50px;
+    width: 120px;
+    border-radius: 5px;
+    margin: 15px;
+    cursor: pointer;
+    background: linear-gradient(5deg, #fae0fad8, #fdc8ecb1);
+    justify-self: end;
+  }
 </style>
