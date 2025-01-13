c39b377c09e8a7134923bb7e4d0bfc07ad5f87ef
EdvinGantelius <ganteliusedvin@gmail.com>
2025-01-13 17:34:47 +0100

    Updatering

--- src/views/Answers.vue
+++ src/views/Answers.vue
@@ -255,13 +255,11 @@
         socket.emit("stopMusic", this.pollId);
         this.questionAnswer = true;
         socket.emit("startQuestions", this.pollId);
-        //this.resetAnswers();
       },
   
       goToScores(){
         socket.emit("showScores", this.pollId);
         this.$router.push('/points/' + this.pollId);
-        //this.resetAnswers();
       },
   
       updatePoints(user, points) {
@@ -272,11 +270,6 @@
         });
       },
   
-      /*resetAnswers() {
-        this.destinationAnswers = [];
-        this.questionAnswers = [];
-        console.log("Tidigare resa och svar är rensade")
-      },*/
       goToSummary () {
         socket.emit("showScores", this.pollId);
         this.$router.push(`/podium/${this.pollId}`);
